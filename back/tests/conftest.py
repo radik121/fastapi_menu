8c4f5e2207d8db213ea3b926d8400a0e9a7605fa
Radik Masagutov <radik121@mail.ru>
2023-02-06 02:17:00 +0300

    refactoring a tetst into async code

--- back/tests/conftest.py
+++ back/tests/conftest.py
@@ -1,97 +1,107 @@
-import pytest
+import asyncio
+
+import pytest_asyncio
 from app import app
 from config import PG_DB_TEST, PG_HOST_TEST, POSTGRES_PASSWORD, POSTGRES_USER
 from db import Base
 from db.engine import get_session
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy_utils import create_database, database_exists
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{PG_HOST_TEST}/{PG_DB_TEST}"
+SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{PG_HOST_TEST}/{PG_DB_TEST}"
 
 
-@pytest.fixture(scope="session")
-def db_engine():
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+@pytest_asyncio.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(scope="session")
+async def db_engine():
+    engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
     if not database_exists:
         create_database(engine.url)
 
-    Base.metadata.create_all(bind=engine)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
     yield engine
 
 
-@pytest.fixture(scope="function")
-def db(db_engine):
-    connection = db_engine.connect()
-    transaction = connection.begin()
-    db = Session(bind=connection)
+@pytest_asyncio.fixture(scope="function")
+async def db(db_engine):
+    connection = await db_engine.connect()
+    transaction = await connection.begin()
+    db = AsyncSession(bind=connection)
 
     yield db
 
-    db.close()
-    transaction.rollback()
-    connection.close()
+    # db.close()
+    await transaction.rollback()
+    await connection.close()
 
 
-@pytest.fixture(scope="function")
-def client(db):
+@pytest_asyncio.fixture(scope="function")
+async def client(db):
     app.dependency_overrides[get_session] = lambda: db
-    with TestClient(app) as c:
+    async with AsyncClient(app=app, base_url="http://test") as c:
         yield c
 
 
-def get_first_menu(url, client):
-    return client.get(url).json()[0]
+async def get_first_menu(url, client):
+    return await client.get(url).json()[0]
 
 
-@pytest.fixture(scope="function")
+@pytest_asyncio.fixture(scope="function")
 def get_menu():
     return get_first_menu
 
 
-@pytest.fixture(scope="function")
-def create_menu(client):
+@pytest_asyncio.fixture(scope="function")
+async def create_menu(client):
     base_url = "/api/v1/menus"
     menu_data = {
         "title": "menu1",
         "description": "desc1",
     }
-    menu = client.post(base_url, json=menu_data)
+    menu = await client.post(base_url, json=menu_data)
 
     return menu.json()
 
 
-@pytest.fixture(scope="function")
-def base_url(client):
+@pytest_asyncio.fixture(scope="function")
+async def base_url(client):
     base_url = "/api/v1/menus"
     menu = {
         "title": "menu1",
         "description": "desc1",
     }
-    menu_id = client.post(base_url, json=menu).json()["id"]
+    response = await client.post(base_url, json=menu)
+    menu_id = response.json()["id"]
 
     return f"/api/v1/menus/{menu_id}/submenus"
 
-    # client.delete(f'{base_url}/{menu_id}')
 
-
-@pytest.fixture(scope="function")
-def base_url_dish(client):
+@pytest_asyncio.fixture(scope="function")
+async def base_url_dish(client):
     base_url = "/api/v1/menus"
     menu = {
         "title": "menu1",
         "description": "desc1",
     }
-    menu_id = client.post(base_url, json=menu).json()["id"]
+    menu_res = await client.post(base_url, json=menu)
+    menu_id = menu_res.json()["id"]
 
     submenu_url = f"/api/v1/menus/{menu_id}/submenus"
     submenu = {
         "title": "submenu1",
         "description": "desc1",
     }
-    submenu_id = client.post(submenu_url, json=submenu).json()["id"]
+    submenu_res = await client.post(submenu_url, json=submenu)
+    submenu_id = submenu_res.json()["id"]
 
     return f"/api/v1/menus/{menu_id}/submenus/{submenu_id}/dishes"
-
-    # client.delete(f'{base_url}/{menu_id}')
--- back/tests/test_api.py
+++ back/tests/test_api.py
@@ -1,62 +1,71 @@
-from db import models
+import pytest
 
 
 class TestMenus:
     url = "/api/v1/menus"
 
-    def test_ctreate_menu(self, client, db):
+    @pytest.mark.asyncio
+    async def test_ctreate_menu(self, client, db):
         menu_data = {
             "title": "menu1",
             "description": "desc1",
         }
-        response = client.post(self.url, json=menu_data)
-        db_data = db.query(models.Menu).first()
+        response = await client.post(self.url, json=menu_data)
 
         assert response.status_code == 201
-        assert response.json()["title"] == db_data.title
-        assert response.json()["description"] == db_data.description
+        assert response.json()["title"] == menu_data["title"]
+        assert response.json()["description"] == menu_data["description"]
         assert "submenus_count" in response.json() and response.json()["submenus_count"] == 0
         assert "dishes_count" in response.json() and response.json()["dishes_count"] == 0
 
-    def test_list_menu(self, client, create_menu):
-        response = client.get(self.url)
+    @pytest.mark.asyncio
+    async def test_list_menu(self, client, create_menu):
+        response = await client.get(self.url)
 
         assert response.status_code == 200
         assert isinstance(response.json(), list)
         assert response.json()[0]["title"] == create_menu["title"]
         assert response.json()[0]["description"] == create_menu["description"]
 
-    def test_get_menu(self, client, db, create_menu):
-        menu = db.query(models.Menu).first()
-        response = client.get(f"{self.url}/{menu.id}")
+    @pytest.mark.asyncio
+    async def test_get_menu(self, client, db, create_menu):
+        menu = await client.get(self.url)
+
+        # print('menu: ', menu.json()[0]['title'])
+        response = await client.get(f"{self.url}/{menu.json()[0]['id']}")
+        print(response.json()["title"])
 
         assert response.status_code == 200
         assert len(response.json()) == 5
-        assert response.json()["title"] == menu.title
+        assert response.json()["title"] == create_menu["title"]
 
-        error_resp = client.get(f"{self.url}/0")
+        error_resp = await client.get(f"{self.url}/0")
 
         assert error_resp.status_code == 404
         assert error_resp.json() == {"detail": "menu not found"}
 
-    def test_update_menu(self, client, db, create_menu):
-        menu = db.query(models.Menu).first()
+    @pytest.mark.asyncio
+    async def test_update_menu(self, client, db, create_menu):
+        menu = await client.get(self.url)
+        menu_id = menu.json()[0]["id"]
         update_data = {"title": "my_menu"}
 
-        response = client.patch(f"{self.url}/{menu.id}", json=update_data)
+        response = await client.patch(f"{self.url}/{menu_id}", json=update_data)
 
         assert response.status_code == 200
         assert response.json()["title"] == update_data["title"]
 
-        error_resp = client.patch(f"{self.url}/0", json=update_data)
+        error_resp = await client.patch(f"{self.url}/0", json=update_data)
 
         assert error_resp.status_code == 404
         assert error_resp.json() == {"detail": "menu not found"}
 
-    def test_delete_menu(self, client, get_menu, db, create_menu):
-        menu = db.query(models.Menu).first()
+    @pytest.mark.asyncio
+    async def test_delete_menu(self, client, get_menu, db, create_menu):
+        menu = await client.get(self.url)
+        menu_id = menu.json()[0]["id"]
 
-        response = client.delete(f"{self.url}/{menu.id}")
+        response = await client.delete(f"{self.url}/{menu_id}")
 
         assert response.status_code == 200
         assert response.json() == {
@@ -71,48 +80,56 @@ class TestSubmenus:
         "description": "desc1",
     }
 
-    def test_list_submenu(self, base_url, client):
-        response = client.get(base_url)
+    @pytest.mark.asyncio
+    async def test_list_submenu(self, base_url, client):
+        response = await client.get(base_url)
 
         assert response.status_code == 200
         assert response.json() == []
 
-    def test_create_submenu(self, base_url, client):
-        response = client.post(base_url, json=self.submenu_data)
+    @pytest.mark.asyncio
+    async def test_create_submenu(self, base_url, client):
+        response = await client.post(base_url, json=self.submenu_data)
 
         assert response.status_code == 201
         assert response.json()["title"] == self.submenu_data["title"]
         assert response.json()["description"] == self.submenu_data["description"]
 
-    def test_get_submenu(self, base_url, client):
-        submenu_id = client.post(base_url, json=self.submenu_data).json()["id"]
-        response = client.get(f"{base_url}/{submenu_id}")
+    @pytest.mark.asyncio
+    async def test_get_submenu(self, base_url, client):
+        submenu_res = await client.post(base_url, json=self.submenu_data)
+        submenu_id = submenu_res.json()["id"]
+        response = await client.get(f"{base_url}/{submenu_id}")
 
         assert response.status_code == 200
         assert len(response.json()) == 4
         assert response.json()["dishes_count"] == 0
 
-        error_resp = response = client.get(f"{base_url}/0")
+        error_resp = await client.get(f"{base_url}/0")
 
         assert error_resp.status_code == 404
         assert error_resp.json() == {"detail": "submenu not found"}
 
-    def test_update_submenu(self, base_url, client):
+    @pytest.mark.asyncio
+    async def test_update_submenu(self, base_url, client):
         update_data = {"title": "new_submenu"}
-        submenu_id = client.post(base_url, json=self.submenu_data).json()["id"]
-        response = client.patch(f"{base_url}/{submenu_id}", json=update_data)
+        submenu_res = await client.post(base_url, json=self.submenu_data)
+        submenu_id = submenu_res.json()["id"]
+        response = await client.patch(f"{base_url}/{submenu_id}", json=update_data)
 
         assert response.status_code == 200
         assert response.json()["title"] == update_data["title"]
 
-        error_resp = response = client.get(f"{base_url}/0")
+        error_resp = await client.get(f"{base_url}/0")
 
         assert error_resp.status_code == 404
         assert error_resp.json() == {"detail": "submenu not found"}
 
-    def test_delete_submenu(self, base_url, client):
-        submenu_id = client.post(base_url, json=self.submenu_data).json()["id"]
-        response = client.delete(f"{base_url}/{submenu_id}")
+    @pytest.mark.asyncio
+    async def test_delete_submenu(self, base_url, client):
+        submenu_res = await client.post(base_url, json=self.submenu_data)
+        submenu_id = submenu_res.json()["id"]
+        response = await client.delete(f"{base_url}/{submenu_id}")
 
         assert response.status_code == 200
         assert response.json() == {
@@ -128,52 +145,60 @@ class TestDishes:
         "price": "13.5",
     }
 
-    def test_list_dish(self, base_url_dish, client):
-        response = client.get(base_url_dish)
+    @pytest.mark.asyncio
+    async def test_list_dish(self, base_url_dish, client):
+        response = await client.get(base_url_dish)
 
         assert response.status_code == 200
         assert response.json() == []
 
-    def test_create_dish(self, base_url_dish, client):
-        response = client.post(base_url_dish, json=self.dish_data)
+    @pytest.mark.asyncio
+    async def test_create_dish(self, base_url_dish, client):
+        response = await client.post(base_url_dish, json=self.dish_data)
 
         assert response.status_code == 201
         assert response.json()["title"] == self.dish_data["title"]
         assert response.json()["description"] == self.dish_data["description"]
 
-    def test_get_menu(self, base_url_dish, client):
-        dish_id = client.post(base_url_dish, json=self.dish_data).json()["id"]
-        response = client.get(f"{base_url_dish}/{dish_id}")
+    @pytest.mark.asyncio
+    async def test_get_menu(self, base_url_dish, client):
+        dish_res = await client.post(base_url_dish, json=self.dish_data)
+        dish_id = dish_res.json()["id"]
+        response = await client.get(f"{base_url_dish}/{dish_id}")
 
         assert response.status_code == 200
         assert len(response.json()) == 4
         assert isinstance(response.json()["price"], str)
 
-        error_resp = client.get(f"{base_url_dish}/0")
+        error_resp = await client.get(f"{base_url_dish}/0")
 
         assert error_resp.status_code == 404
         assert error_resp.json() == {"detail": "dish not found"}
 
-    def test_update_dish(self, base_url_dish, client):
+    @pytest.mark.asyncio
+    async def test_update_dish(self, base_url_dish, client):
         update_data = {
             "title": "new_dish",
             "price": "134.50",
         }
-        dish_id = client.post(base_url_dish, json=self.dish_data).json()["id"]
-        response = client.patch(f"{base_url_dish}/{dish_id}", json=update_data)
+        dish_res = await client.post(base_url_dish, json=self.dish_data)
+        dish_id = dish_res.json()["id"]
+        response = await client.patch(f"{base_url_dish}/{dish_id}", json=update_data)
 
         assert response.status_code == 200
         assert response.json()["title"] == update_data["title"]
         assert response.json()["price"] == update_data["price"]
 
-        error_resp = client.get(f"{base_url_dish}/0")
+        error_resp = await client.get(f"{base_url_dish}/0")
 
         assert error_resp.status_code == 404
         assert error_resp.json() == {"detail": "dish not found"}
 
-    def test_delete_dish(self, base_url_dish, client):
-        dish_id = client.post(base_url_dish, json=self.dish_data).json()["id"]
-        response = client.delete(f"{base_url_dish}/{dish_id}")
+    @pytest.mark.asyncio
+    async def test_delete_dish(self, base_url_dish, client):
+        dish_res = await client.post(base_url_dish, json=self.dish_data)
+        dish_id = dish_res.json()["id"]
+        response = await client.delete(f"{base_url_dish}/{dish_id}")
 
         assert response.status_code == 200
         assert response.json() == {
